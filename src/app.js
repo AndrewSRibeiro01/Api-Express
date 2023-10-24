d309d0a5b6b373d3b31220d6dce3d61849164186
Andrew Souza <andrew.souza@aigroup.local>
2023-10-24 17:49:14 -0300

    fix: atlas mongodb postman e marcha

--- src/app.js
+++ src/app.js
@@ -1,37 +1,33 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./modelos/Livro.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro)
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso");
+});
 
 const app = express();
 app.use(express.json());
-
-const livros = [
-    {
-        id: 1,
-        titulo: "senhor dos anéis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })
-}
 
 app.get("/", (req, res) => {
     res.status(200).send("Gaguelson de ilhelson!!!");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
     res.status(200).json(livros[index]);
-})
+});
 
 app.post("/livros", (req, res) => {
     livros.push(req.body);
@@ -48,6 +44,6 @@ app.delete("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
     livros.splice(index, 1);
     res.status(200).send("livro removido com sucesso")
-})
+});
 
 export default app;
---
+++ src/config/dbConnect.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose"
+
+async function conectaNaDatabase() {
+    mongoose.connect("mongodb+srv://admin:admin123@cluster0.uhkojdn.mongodb.net/livraria?retryWrites=true&w=majority");
+
+    return mongoose.connection;
+};
+
+export default conectaNaDatabase;
